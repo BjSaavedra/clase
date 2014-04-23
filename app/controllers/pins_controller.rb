77fc2bb0440dbc318cf250e0f08d47d6624ee303
BjSaavedra <bsaavedra@live.cl>
2014-04-23 00:11:03 -0300

    viwes mod for user (:edit :update :destroy)

--- app/controllers/pins_controller.rb
+++ app/controllers/pins_controller.rb
@@ -1,5 +1,5 @@
 class PinsController < ApplicationController
-  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+  before_action :set_pin, only: [:show]
   before_filter :authenticate_user!, only: [:show]
 
   def macroeconomia
